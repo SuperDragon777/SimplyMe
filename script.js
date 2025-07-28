c6edb5e720cc0987e8d7379dcb482700dc02e5a9
SuperDragon777 <Vadim.muromtsev@gmail.com>
2025-07-28 18:23:36 +0300

    Mobile site optimization 2
    Improvements...

--- script.js
+++ script.js
@@ -10,12 +10,16 @@ window.addEventListener("scroll", function () {
 const element = document.getElementById("hom");
 const element2 = document.getElementById("hom2");
 const element3 = document.getElementById("only_m");
+const wall = document.getElementById("wall");
+const header_container = document.getElementById("header-container");
     document.addEventListener('DOMContentLoaded', function() {
     element3.style.display = 'none'
     if (/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         element.style.display = 'none'
         element2.style.display = 'none'
         element3.style.display = 'block'
+        wall.style.maxHeight = "300px"
+        header_container.style.justifyContent = "center"
 }
 });
 
